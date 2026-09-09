05f589a950075bc8db3c4071c74ce4e144ba1880
Kamylla Florencio <kamylla.florencio@gmail.com>
2026-09-09 15:13:42 -0700

    Add initial Selenium smoke test

--- src/test/java/tests/SauceDemoSmokeTest.java
+++ src/test/java/tests/SauceDemoSmokeTest.java
@@ -1,4 +1,34 @@
 package tests;
 
-public class SauceDemoSmokeTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SauceDemoSmokeTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void shouldOpenSauceDemo() {
+        driver.get("https://www.saucedemo.com/");
+
+        assertEquals("Swag Labs", driver.getTitle());
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
