802bd5a1e04b623984be4457cd76da88bf2db33c
Kamylla Florencio <kamylla.florencio@gmail.com>
2026-09-14 14:12:40 -0700

    Refactor tests to use shared base setup

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,33 +1,18 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
 
-    private WebDriver driver;
     private LoginPage loginPage;
 
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-
+    void initializeLoginPage() {
         loginPage = new LoginPage(driver);
-    }
-
-    @AfterEach
-    void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
     }
 
     @Test
